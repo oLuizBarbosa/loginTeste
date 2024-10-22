7110e4d49a3cd1f13c7a437794af1dcf9c618df2
João Víctor <joao002miranda@gmail.com>
2024-10-22 14:23:50 -0300

    Ajustando algumas coisas na tela

--- view/exibirLista.php
+++ view/exibirLista.php
@@ -4,14 +4,16 @@
         <div class="conteudo-principal">
             <h2>Lista de Tarefas</h2>
             <!-- Conteúdo da lista de tarefas vai aqui -->
+
+
+            
         </div>
 
         <!-- Seção Menor à Direita -->
         <div class="barra-lateral">
-            <h3>Minhas Listas</h3>
-            <div class="listas-de-tarefa"> </div>
-            <button>Criar Nova Lista</button>
-            
+                <h2 style="text-align: center; max-height: 100%;">Minhas Listas</h2>
+                <div class="listas-de-tarefa"> </div>
+                <button>Criar Nova Lista</button>
         </div>
     </div>
 
