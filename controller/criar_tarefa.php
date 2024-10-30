950921d3f54c7c1d46bbfe1a95a407fddc93b8aa
João Víctor <joao002miranda@gmail.com>
2024-10-30 09:30:07 -0300

    Revert "Antes da tentativa de mudança na criação de tarefas..."
    This reverts commit d248b0e485c3428200d588e80815b629bcd8fd41.

--- controller/criar_tarefa.php
+++ controller/criar_tarefa.php
@@ -10,11 +10,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $descricao_tarefa = $_POST['descricao_tarefa'];
         $data_conclusao = $_POST['data_final'];
         $id_lista = $_POST['id_lista'];//ISSO DEFINITIVAMENTE NÃO DEVERIA SER ASSIM
-        $situacao = "pendente";
+        
         try {
-            $sql = "INSERT INTO tarefas (nome, descricao, data_conclusao, id_lista_tarefa, situacao, data_criacao) VALUES (?, ?, ?, ?, ?, CURDATE())";
+            $sql = "INSERT INTO tarefas (nome, descricao, data_conclusao, id_lista_tarefa, data_criacao) VALUES (?, ?, ?, ?, CURDATE())";
             $stmt = $conn->prepare($sql);
-            $stmt->execute([$nome_tarefa, $descricao_tarefa, $data_conclusao, $id_lista, $situacao]);
+            $stmt->execute([$nome_tarefa, $descricao_tarefa, $data_conclusao, $id_lista]);
             header('Location: ../view/dashboard.php');
             
         } catch (PDOException $e) {
